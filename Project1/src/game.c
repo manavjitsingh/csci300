4d8f6db6d9697f045c86fbe1a9ef7cfc7e6c8b49
Manavjit Singh <writetomanavjit26@gmail.com>
2025-03-09 02:21:49 +0000

    Part 1 completed

---
+++ Project1/src/game.c
@@ -0,0 +1,100 @@
+#include "game.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#include "linked_list.h"
+#include "mbstrings.h"
+#include "common.h"
+
+/** Updates the game by a single step, and modifies the game information
+ * accordingly. Arguments:
+ *  - cells: a pointer to the first integer in an array of integers representing
+ *    each board cell.
+ *  - width: width of the board.
+ *  - height: height of the board.
+ *  - snake_p: pointer to your snake struct (not used until part 3!)
+ *  - input: the next input.
+ *  - growing: 0 if the snake does not grow on eating, 1 if it does.
+ */
+void update(int* cells, size_t width, size_t height, snake_t* snake_p,
+            enum input_key input, int growing) {
+    // `update` should update the board, your snake's data, and global
+    // variables representing game information to reflect new state. If in the
+    // updated position, the snake runs into a wall or itself, it will not move
+    // and global variable g_game_over will be 1. Otherwise, it will be moved
+    // to the new position. If the snake eats food, the game score (`g_score`)
+    // increases by 1. This function assumes that the board is surrounded by
+    // walls, so it does not handle the case where a snake runs off the board.
+    
+    // TODO: implement!
+    // printf("width = %zu, height = %zu, cells[42] = %d\n", width, height, cells[42]);
+    size_t snakeIndex;
+    // size_t grassCell;
+    for(size_t i=0; i<width*height; i++){
+        if (cells[i] & FLAG_SNAKE) {  // Check if the FLAG_SNAKE bit is set.
+        snakeIndex = i;           // Found a cell with the flag.
+        break;                    // Stop after the first one is found.
+        }
+    }
+    if(cells[snakeIndex + 1] & FLAG_WALL){
+        g_game_over = 1;
+    }
+    else{
+        cells[snakeIndex + 1] |= FLAG_SNAKE;
+        snake_position = cells + snakeIndex + 1;
+        cells[snakeIndex] &= ~FLAG_SNAKE; 
+    }
+    // for (size_t i = 0; i < width*height; i++)
+    // {
+    //     if((cells[i] == FLAG_GRASS) && (cells[i] == FLAG_SNAKE)){
+    //         cells[i] |= FLAG_SNAKE;
+    //     }
+    // } 
+}
+
+/** Sets a random space on the given board to food.
+ * Arguments:
+ *  - cells: a pointer to the first integer in an array of integers representing
+ *    each board cell.
+ *  - width: the width of the board
+ *  - height: the height of the board
+ */
+void place_food(int* cells, size_t width, size_t height) {
+    /* DO NOT MODIFY THIS FUNCTION */
+    unsigned food_index = generate_index(width * height);
+    // check that the cell is empty or only contains grass
+    if ((*(cells + food_index) == PLAIN_CELL) || (*(cells + food_index) == FLAG_GRASS)) {
+        *(cells + food_index) |= FLAG_FOOD;
+    } else {
+        place_food(cells, width, height);
+    }
+    /* DO NOT MODIFY THIS FUNCTION */
+}
+
+/** Prompts the user for their name and saves it in the given buffer.
+ * Arguments:
+ *  - `write_into`: a pointer to the buffer to be written into.
+ */
+void read_name(char* write_into) {
+    // TODO: implement! (remove the call to strcpy once you begin your
+    // implementation)
+    strcpy(write_into, "placeholder");
+}
+
+/** Cleans up on game over — should free any allocated memory so that the
+ * LeakSanitizer doesn't complain.
+ * Arguments:
+ *  - cells: a pointer to the first integer in an array of integers representing
+ *    each board cell.
+ *  - snake_p: a pointer to your snake struct. (not needed until part 3)
+ */
+void teardown(int* cells, snake_t* snake_p) {  
+    // TODO: Free the board
+    free(cells);
+    // TODO: implement! (part 3A)
+}
